c4e7982880947072d4a5c9f09a84c56eafbdbb93
Alina K <playpurplepiano@gmail.com>
2018-02-06 02:33:10 +0300

    design faq page

---
+++ faq.php
@@ -0,0 +1,23 @@
+<? 
+
+include 'header.php';
+
+?>
+
+	<div class="faq-page">
+		<div class="item">
+			<div class="question">
+				вопрос
+			</div>
+
+			<div class="answer">
+				ответ
+			</div>
+		</div>
+	</div>
+
+<? 
+
+include 'footer.php';
+
+?>
